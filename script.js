ea29df6935717ab4c26519e95999013bb3357b62
Eder Canada <canada.eder@gmail.com>
2021-09-20 09:48:30 -0700

    esc key event

--- script.js
+++ script.js
@@ -5,21 +5,30 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
 
-console.log(btnsOpenModal);
-
 function closeModal() {
   modal.classList.add('hidden');
   overlay.classList.add('hidden');
 }
 
+function openModal() {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+}
+
 for (let i = 0; i < btnsOpenModal.length; i++) {
-  btnsOpenModal[i].addEventListener('click', function () {
-    console.log('hit');
-    modal.classList.remove('hidden');
-    overlay.classList.remove('hidden');
-  });
+  btnsOpenModal[i].addEventListener('click', openModal);
 }
 
 btnCloseModal.addEventListener('click', closeModal);
 
 overlay.addEventListener('click', closeModal);
+
+
+document.addEventListener('keydown', function(e){
+  if(e.key === "Escape"){
+    closeModal()
+  }
+})
+
+
+
